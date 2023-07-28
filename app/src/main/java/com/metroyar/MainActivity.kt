f249485fc64f06f08c73c8b1c5c95cdfc8af393f
Amin <amin.sakha00@gmail.com>
2023-07-28 20:48:25 +0330

    create infrastructure for Bottom Nav

--- app/src/main/java/com/metroyar/MainActivity.kt
+++ app/src/main/java/com/metroyar/MainActivity.kt
@@ -3,44 +3,109 @@ package com.metroyar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.exyte.animatednavbar.AnimatedNavigationBar
+import com.exyte.animatednavbar.animation.balltrajectory.Parabolic
+import com.exyte.animatednavbar.animation.balltrajectory.Straight
+import com.exyte.animatednavbar.animation.balltrajectory.Teleport
+import com.exyte.animatednavbar.animation.indendshape.Height
+import com.exyte.animatednavbar.animation.indendshape.shapeCornerRadius
 import com.metroyar.ui.theme.MetroYarTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MetroYarTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            DestinationsNavHost(navGraph = NavGraphs.root)
+
         }
     }
 }
 
+@RootNavGraph(start = true)
+@Destination
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Greeting() {
+    var selectedIndex by remember { mutableStateOf(0) }
+    //val navBarItems = remember { NavBarItems.values() }
+
+    Scaffold(
+        modifier = Modifier.padding(all = 12.dp),
+        bottomBar = {
+            AnimatedNavigationBar(
+                selectedIndex = selectedIndex,
+                modifier = Modifier.height(64.dp),
+                cornerRadius = shapeCornerRadius(cornerRadius = 34.dp),
+                ballAnimation = Parabolic(tween(300)),
+                indentAnimation = Height(tween(600)),
+                barColor = MaterialTheme.colorScheme.primary,
+                ballColor = MaterialTheme.colorScheme.primary
+            ) {
+//                navBarItems.forEach {
+//                    Box(
+//                        modifier = Modifier
+//                            .fillMaxSize()
+//                            .nonRipple { selectedIndex = it.ordinal },
+//                        contentAlignment = Alignment.Center
+//                    ) {
+//                        Icon(
+//                            modifier = Modifier.size(26.dp),
+//                            imageVector = it.icon,
+//                            contentDescription = "",
+//                            tint = if (selectedIndex == it.ordinal) MaterialTheme.colorScheme.error
+//                            else MaterialTheme.colorScheme.inversePrimary
+//                        )
+//                    }
+//                }
+            }
+        }, content = { padding ->
+            Column(
+                modifier = Modifier
+                    .padding(padding)
+            ) {}
+        }
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MetroYarTheme {
-        Greeting("Android")
+fun Modifier.nonRipple(onclick: () -> Unit): Modifier = composed {
+    clickable(
+        indication = null,
+        interactionSource = remember {
+            MutableInteractionSource()
+        }) {
+        onclick()
     }
 }
