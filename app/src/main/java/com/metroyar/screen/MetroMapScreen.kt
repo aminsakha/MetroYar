e7adfe655931f8baab362bf14897ddfdd8a0293f
Amin <amin.sakha00@gmail.com>
2023-08-10 17:34:02 +0330

    the image of metro map is now shown in the corresponding screen

--- app/src/main/java/com/metroyar/screen/MetroMapScreen.kt
+++ app/src/main/java/com/metroyar/screen/MetroMapScreen.kt
@@ -1,13 +1,37 @@
 package com.metroyar.screen
 
+
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.metroyar.R
+import net.engawapg.lib.zoomable.ZoomState
+import net.engawapg.lib.zoomable.rememberZoomState
+import net.engawapg.lib.zoomable.zoomable
 
 @Composable
 fun MetroMapScreen() {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Text("map")
+        ZoomableImage(drawable = R.drawable.metro_map)
     }
+}
+
+@Composable
+fun ZoomableImage(drawable: Int) {
+    val painter = painterResource(id = drawable)
+    val zoomState = rememberZoomState(contentSize = painter.intrinsicSize)
+    Image(
+        painter = painter,
+        contentDescription = "Zoomable image",
+        contentScale = ContentScale.Fit,
+        modifier = Modifier
+            .fillMaxSize()
+            .zoomable(zoomState),
+    )
 }
--- app/src/main/java/com/metroyar/utils/GlobalObjects.kt
+++ app/src/main/java/com/metroyar/utils/GlobalObjects.kt
@@ -1,13 +1,16 @@
 package com.metroyar.utils
 
+import androidx.compose.runtime.mutableStateOf
 import com.metroyar.classes.MetroGraph
 import com.metroyar.model.Station
+import net.engawapg.lib.zoomable.ZoomState
 
 object GlobalObjects {
     const val TAG = "testMetroYar"
     val adjNodesLineNum = mutableMapOf<Pair<Int, Int>, Int>()
     val stationList = mutableListOf<Station>()
     val metroGraph = MetroGraph(151)
+
      val tripleOfLinesAndTheirStartAndEndStations =
         mutableListOf<Triple<Int, String, String>>().apply {
             add(Triple(1, "تجریش", "کهریزک"))
