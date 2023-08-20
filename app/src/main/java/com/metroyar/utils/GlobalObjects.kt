fc059e4668d95fd3b022ee06c49a695b118a1d59
Amin <amin.sakha00@gmail.com>
2023-08-20 23:44:28 +0330

    fix some end and first node of lines

--- app/src/main/java/com/metroyar/utils/GlobalObjects.kt
+++ app/src/main/java/com/metroyar/utils/GlobalObjects.kt
@@ -1,13 +1,8 @@
 package com.metroyar.utils
 
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.focus.FocusRequester
 import com.metroyar.classes.MetroGraph
 import com.metroyar.model.Station
-import net.engawapg.lib.zoomable.ZoomState
 
 object GlobalObjects {
     const val TAG = "testMetroYar"
@@ -22,9 +17,9 @@ object GlobalObjects {
             add(Triple(1, "تجریش", "کهریزک"))
             add(Triple(2, "فرهنگسرا", "تهران (صادقیه)"))
             add(Triple(3, "قائم", "آزادگان"))
-            add(Triple(4, "شهید کلاهدوز", "ارم سبز"))
-            add(Triple(5, "تهران (صادقیه)", "شهید سپهبد قاسم سلیمانی"))
+            add(Triple(4, "شهید کلاهدوز", "علامه جعفری"))
+            add(Triple(5, "تهران (صادقیه)", "شهید سپهبد قاسم سلیمانی(هشتگرد)"))
             add(Triple(6, "شهید ستاری", "دولت آباد"))
-            add(Triple(7, "میدان صنعت", "بسیج"))
+            add(Triple(7, "شهید دادمان", "بسیج"))
         }
 }
