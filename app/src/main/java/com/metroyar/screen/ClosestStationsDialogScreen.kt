f3afd45adb68becc5d45396f2283ba529e33210a
Amin <amin.sakha00@gmail.com>
2023-10-05 20:29:22 +0330

    improve Neshan api calling

--- app/src/main/java/com/metroyar/screen/ClosestStationsDialogScreen.kt
+++ app/src/main/java/com/metroyar/screen/ClosestStationsDialogScreen.kt
@@ -110,7 +110,7 @@ fun ClosestStationsDialogScreen(
     }
 
     if (closestStationsPair.first != "empty") {
-        log("res from Neshan", closestStationsPair)
         clipSpec = LottieClipSpec.Progress(0.0f, 1f)
+        log("res from Neshan", closestStationsPair)
     }
 }
