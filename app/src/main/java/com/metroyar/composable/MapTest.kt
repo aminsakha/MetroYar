db8d804b29b6c461f3b05f17aefa7013bf3c417e
Amin <amin.sakha00@gmail.com>
2023-12-06 23:50:33 +0330

    changed the style

--- app/src/main/java/com/metroyar/composable/MapTest.kt
+++ app/src/main/java/com/metroyar/composable/MapTest.kt
@@ -3,26 +3,29 @@ package com.metroyar.composable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.MapInitOptions
+import com.mapbox.maps.MapboxExperimental
+import com.mapbox.maps.Style
 import com.mapbox.maps.extension.compose.MapboxMap
-import com.mapbox.maps.extension.compose.animation.viewport.MapViewportState
 import com.ramcosta.composedestinations.annotation.Destination
 
-
+@OptIn(MapboxExperimental::class)
 @Destination
 @Composable
 fun MapTest() {
-    val context = LocalContext.current
     MapboxMap(
-        Modifier.fillMaxSize(),
-        mapViewportState = MapViewportState().apply {
-            setCameraOptions {
-                zoom(15.0)
-                center(Point.fromLngLat( 51.388740,35.714045))
-                pitch(0.0)
-                bearing(0.0)
-            }
-        },
+        modifier = Modifier.fillMaxSize(),
+        mapInitOptionsFactory = { context ->
+            MapInitOptions(
+                context = context,
+                styleUri = Style.TRAFFIC_DAY,
+                cameraOptions = CameraOptions.Builder()
+                    .center(Point.fromLngLat(51.388740,35.714045))
+                    .zoom(15.0)
+                    .build()
+            )
+        }
     )
 }
